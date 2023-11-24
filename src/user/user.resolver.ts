38dcdeea6c272a55af16fbdd301564aa0d0cbc81
lee muna <muna@retgames.co.kr>
2023-11-24 14:17:39 +0900

    📦 chore(package.json): class-transformer 및 class-validator 패키지 추가 📄 추가(create-user.input.ts): CreateUserInput DTO 추가 🔧 수정(user.resolver.ts): createUser 뮤테이션 추가 및 userService 주입

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,23 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private userService: UserService) {}
+
+  @Mutation((returns) => User)
+  async createUser(
+    @Args('createUserData') createUserData: CreateUserInput,
+  ): Promise<User> {
+    try {
+      // Validate createUserData if necessary
+
+      const user = await this.userService.createUser(createUserData);
+      return user;
+    } catch (error) {
+      // Handle or rethrow the error based on your error handling strategy
+      throw new Error('Error creating user: ' + error.message);
+    }
+  }
+}
