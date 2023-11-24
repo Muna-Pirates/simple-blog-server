13f410805e53ed81948e03f5ed57d18461af29c4
lee muna <muna@retgames.co.kr>
2023-11-24 14:14:40 +0900

    🔧 chore(package.json): bcrypt 및 @types/bcrypt 패키지 추가 🐛 fix(user.service.ts): createUser 및 findUser 메서드에 예외 처리 추가 ✨ 피트(user.service.ts): updateUser 및 deleteUser 메서드에 예외 처리 추가

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,58 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from 'src/common/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async createUser(data: Prisma.UserCreateInput): Promise<User> {}
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const user = await this.prisma.user.create({
+      data: {
+        ...data,
+        password: hashedPassword,
+      },
+    });
+    return user;
+  }
 
-  async findUser(userId: number): Promise<User | null> {}
+  async findUser(userId: number): Promise<User | null> {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
 
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+
+    return user;
+  }
   async updateUser(params: {
     userId: number;
     data: Prisma.UserUpdateInput;
-  }): Promise<User> {}
+  }): Promise<User> {
+    const { userId, data } = params;
+    const existingUser = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+    if (!existingUser) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
 
-  async deleteUser(userId: number): Promise<User> {}
+    return this.prisma.user.update({
+      where: { id: userId },
+      data,
+    });
+  }
+  async deleteUser(userId: number): Promise<User> {
+    const user = await this.prisma.user.findUnique({ where: { id: userId } });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found.`);
+    }
+
+    return this.prisma.user.delete({ where: { id: userId } });
+  }
 }
