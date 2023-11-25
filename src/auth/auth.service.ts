00190eb36d10031d4b460c04d12dfc93d736ec44
munawiki <munawiki@gmail.com>
2023-11-25 12:52:03 +0900

    🔧 fix(app.module.ts): add AuthService to the providers array to make it available for dependency injection 🔧 fix(user.resolver.ts): import AuthService and AuthPayload to be able to use them in the resolver 🔧 fix(user.resolver.ts): import LoginInput to be able to use it in the resolver 🔧 fix(user.resolver.ts): add authService to the constructor to be able to use it in the resolver ✨ feat(auth.service.ts): create AuthService to handle user authentication and token generation ✨ feat(auth.service.spec.ts): add unit test for AuthService to test if it is defined ✨ feat(auth.service.ts): add validateUser method to validate user credentials ✨ feat(auth.service.ts): add generateJwtToken method to generate JWT token for authenticated user ✨ feat(auth-payload.dto.ts): create AuthPayload DTO to represent the authentication payload ✨ feat(login-input.dto.ts): create LoginInput DTO to represent the login input data 🔧 fix(create-user.input.ts): remove IsOptional decorator from roleId field to make it required ✨ feat(user.resolver.ts): add registerUser mutation to register a new user

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from 'src/common/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (user && (await bcrypt.compare(password, user.password))) {
+        return user;
+      }
+
+      return null;
+    } catch (error) {
+      throw new Error('Error validating user');
+    }
+  }
+
+  async generateJwtToken(user: User): Promise<string> {
+    const payload = { username: user.email, sub: user.id };
+    return this.jwtService.sign(payload);
+  }
+}
