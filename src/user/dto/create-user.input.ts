4e516f1e624a4e6dc7a2361c397a6ef4cd0e34bf
munawiki <munawiki@gmail.com>
2023-11-25 12:14:16 +0900

    🔧 chore(create-user.input.ts): update import statements and add validation messages for email and password fields 🔧 chore(create-user.input.ts): add optional 'name' field to CreateUserInput 🔧 chore(user.types.ts): rename UserType to User and update field names and types 🔧 chore(user.resolver.ts): update registerUser mutation to validate password length and check for existing user by email 🔧 chore(user.resolver.ts): hash password before creating user 🔧 chore(user.service.ts): add findByEmail method to UserService to find user by email

--- src/user/dto/create-user.input.ts
+++ src/user/dto/create-user.input.ts
@@ -1,19 +1,21 @@
-// /path/to/dto/create-user.input.ts
+// src/user/dto/create-user.input.ts
 
-import { Field, InputType } from '@nestjs/graphql';
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
+import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
 
 @InputType()
 export class CreateUserInput {
   @Field()
-  @IsNotEmpty()
-  username: string;
-
-  @Field()
-  @IsEmail()
+  @IsEmail({}, { message: 'Invalid email format' })
   email: string;
 
   @Field()
-  @MinLength(6)
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
+
+  @Field({ nullable: true })
+  @IsString()
+  @IsOptional()
+  name?: string;
 }
