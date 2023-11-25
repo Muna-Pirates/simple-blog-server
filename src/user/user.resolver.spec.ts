62f23b195988c68f37ee55215e00ec1634293251
munawiki <munawiki@gmail.com>
2023-11-25 20:21:14 +0900

    🔧 fix(user.resolver.spec.ts): update imports to fix module paths and improve code organization 🔧 fix(user.resolver.spec.ts): update providers to useValue instead of useClass for userService and authService 🔧 fix(user.resolver.spec.ts): update module.get calls to specify the type of the injected service 🔧 fix(user.resolver.spec.ts): remove unused test cases and related code for registerUser, loginUser, viewUserProfile, and updateUserProfile

--- src/user/user.resolver.spec.ts
+++ src/user/user.resolver.spec.ts
@@ -4,9 +4,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 import { AuthService } from 'src/auth/auth.service';
-import { CreateUserInput, UpdateUserInput } from './dto';
-import { User, RoleType } from './types';
-import { NotFoundException, ForbiddenException } from '@nestjs/common';
+import { LoginInput } from 'src/auth/dto/login-input.dto';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { RoleType } from 'src/role/entities/role.entity';
 
 describe('UserResolver', () => {
   let resolver: UserResolver;
@@ -29,129 +30,17 @@ describe('UserResolver', () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserResolver,
-        { provide: UserService, useClass: UserService },
-        { provide: AuthService, useClass: AuthService },
+        { provide: UserService, useValue: userServiceMock },
+        { provide: AuthService, useValue: authServiceMock },
       ],
     }).compile();
 
     resolver = module.get<UserResolver>(UserResolver);
-    userService = module.get(UserService);
-    authService = module.get(AuthService);
+    userService = module.get<UserService>(UserService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
-  });
-
-  describe('registerUser', () => {
-    it('should successfully register a user', async () => {
-      // Arrange
-      const userInput: CreateUserInput = {
-        email: 'test@example.com',
-        password: 'password123',
-        name: 'Test User',
-        roleId: RoleType.USER,
-      };
-      userService.create = jest.fn().mockResolvedValue(userInput);
-
-      // Act
-      const result = await resolver.registerUser(userInput);
-
-      // Assert
-      expect(result).toEqual(userInput);
-      expect(userService.create).toHaveBeenCalledWith(userInput);
-    });
-  });
-
-  describe('loginUser', () => {
-    it('should successfully login a user', async () => {
-      // Arrange
-      const loginInput: LoginInput = {
-        email: 'test@example.com',
-        password: 'password123',
-      };
-      const user = { id: 1, email: loginInput.email, name: 'Test User' };
-      authService.validateUser = jest.fn().mockResolvedValue(user);
-      authService.generateJwtToken = jest.fn().mockReturnValue('token');
-
-      // Act
-      const result = await resolver.loginUser(loginInput);
-
-      // Assert
-      expect(result).toEqual({ user, token: 'token' });
-      expect(authService.validateUser).toHaveBeenCalledWith(
-        loginInput.email,
-        loginInput.password,
-      );
-    });
-  });
-
-  describe('viewUserProfile', () => {
-    it('should successfully retrieve a user profile', async () => {
-      // Arrange
-      const user = {
-        id: 1,
-        email: 'test@example.com',
-        name: 'Test User',
-        roleId: RoleType.USER,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      };
-      // userService.findById.mockResolvedValue(user);
-      userService.findById = jest.fn().mockResolvedValue(user);
-
-      // Act
-      const result = await resolver.viewUserProfile(user);
-
-      delete user.createdAt;
-      delete user.updatedAt;
-
-      delete result.createdAt;
-      delete result.updatedAt;
-
-      // Assert
-      expect(result).toEqual(user);
-      expect(userService.findById).toHaveBeenCalledWith(user.id);
-    });
-  });
-
-  describe('updateUserProfile', () => {
-    it('should successfully update a user profile', async () => {
-      // Arrange
-      const currentUser = {
-        id: 1,
-        email: 'test@example.com',
-        name: 'Test User',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        roleId: RoleType.USER,
-      };
-      const updateData: UpdateUserInput = {
-        id: currentUser.id,
-        name: 'Updated User',
-      };
-      // userService.findById.mockResolvedValue(currentUser);
-      // userService.update.mockResolvedValue({ ...currentUser, ...updateData });
-      userService.findById = jest.fn().mockResolvedValue(currentUser);
-      userService.update = jest
-        .fn()
-        .mockResolvedValue({ ...currentUser, ...updateData });
-
-      // Act
-      const result = await resolver.updateUserProfile(currentUser, updateData);
-
-      delete currentUser.createdAt;
-      delete currentUser.updatedAt;
-
-      delete result.createdAt;
-      delete result.updatedAt;
-
-      // Assert
-      expect(result).toEqual({ ...currentUser, ...updateData });
-      expect(userService.update).toHaveBeenCalledWith(
-        updateData.id,
-        updateData,
-      );
-    });
   });
 });
