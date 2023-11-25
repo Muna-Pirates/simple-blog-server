b7725f218fa0336f20e605e2bf62db3f8d382a5d
munawiki <munawiki@gmail.com>
2023-11-25 21:14:25 +0900

    🔧 fix(post.resolver.ts): remove unused import of PrismaService to improve code cleanliness and remove potential confusion 🔧 fix(post.service.ts): add missing import statements for PrismaService and Prisma types to fix compilation errors ✨ feat(post.service.ts): add create method to PostService to handle post creation with proper author connection and include author and comments in the response

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -2,7 +2,6 @@ import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
-import { PrismaService } from 'src/common/prisma.service';
 import { CreatePostInput } from './dto/create-post.input';
 import { User } from 'src/user/types/user.types';
 import { Post } from 'src/post/types/post.types';
@@ -13,16 +12,20 @@ export class PostResolver {
   constructor(private readonly postService: PostService) {}
 
   @Mutation(() => Post)
-  @UseGuards(AuthGuard) // Apply RoleGuard here if needed
+  @UseGuards(AuthGuard)
   async createPost(
     @Args('createPostInput') createPostInput: CreatePostInput,
     @CurrentUser() user: User,
   ): Promise<Post> {
-    // Ensure that the current user's ID is included in the post creation
     const post = await this.postService.create({
       ...createPostInput,
-      authorId: user.id,
+      author: {
+        connect: {
+          id: user.id,
+        },
+      },
     });
+
     return post;
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/common/prisma.service';
+import { Post, Prisma } from '@prisma/client';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createPostInput: Prisma.PostCreateInput): Promise<PrismaPost> {
+    try {
+      const post = await this.prisma.post.create({
+        data: {
+          ...createPostInput,
+        },
+        include: {
+          author: true,
+          comments: true,
+        },
+      });
+
+      return post;
+    } catch (error) {
+      throw new Error(`Error creating post: ${error.message}`);
+    }
+  }
+}
