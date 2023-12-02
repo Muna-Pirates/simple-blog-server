266ad7b0dc2dd1df6103778493ece4f56d871008
munawiki <munawiki@gmail.com>
2023-12-02 18:36:13 +0900

    modify global exception

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -4,7 +4,6 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
-  HttpStatus,
 } from '@nestjs/common';
 import { GqlExceptionFilter } from '@nestjs/graphql';
 import { GraphQLError } from 'graphql';
@@ -19,21 +18,18 @@ export class GlobalExceptionFilter
     let errorResponse;
 
     if (exception instanceof PrismaClientKnownRequestError) {
-      // Uniformly handle Prisma errors
       errorResponse = this.formatErrorResponse(
         `Prisma error: ${exception.message}`,
         'PRISMA_ERROR',
         { prismaCode: exception.code },
       );
     } else if (exception instanceof GraphQLError) {
-      // Uniformly handle GraphQL errors
       errorResponse = this.formatErrorResponse(
         exception.message,
         'GRAPHQL_ERROR',
         exception.extensions,
       );
     } else if (exception instanceof HttpException) {
-      // Uniformly handle HTTP exceptions
       const statusCode = exception.getStatus();
       errorResponse = this.formatErrorResponse(
         exception.getResponse()['message'] || exception.message,
@@ -41,13 +37,11 @@ export class GlobalExceptionFilter
         { statusCode },
       );
     } else if (exception instanceof Error) {
-      // Uniformly handle generic errors
       errorResponse = this.formatErrorResponse(
         exception.message,
         'INTERNAL_SERVER_ERROR',
       );
     } else {
-      // Fallback for unknown error types
       errorResponse = this.formatErrorResponse(
         'Unknown error occurred',
         'INTERNAL_SERVER_ERROR',
