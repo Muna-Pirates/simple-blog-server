b88aacac05719cc96d661ae58345b36c0a80ca8c
munawiki <munawiki@gmail.com>
2023-12-05 21:32:00 +0900

    🔒 chore(login-input.dto.ts): add validation decorators to email and password fields for improved input validation

--- src/auth/dto/login-input.dto.ts
+++ src/auth/dto/login-input.dto.ts
@@ -1,10 +1,14 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsEmail, IsString, MinLength } from 'class-validator';
 
 @InputType()
 export class LoginInput {
   @Field()
+  @IsEmail({}, { message: 'Invalid email format' })
   email: string;
 
   @Field()
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters long' })
   password: string;
 }
