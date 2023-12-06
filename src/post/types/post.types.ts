fc16f8ccd6d950f5255335d7fd1665bd500d8ac2
lee muna <muna@retgames.co.kr>
2023-12-06 13:54:38 +0900

    🔧 수정(post.types.ts): Post 객체에 대한 설명 추가 🔧 수정(post.types.ts): Post 객체의 각 필드에 대한 설명 추가 🔧 수정(schema.gql): Post 객체에 대한 설명 추가 🔧 수정(schema.gql): Post 객체의 각 필드에 대한 설명 추가

--- src/post/types/post.types.ts
+++ src/post/types/post.types.ts
@@ -3,35 +3,44 @@ import { Category } from 'src/category/types/category.types';
 import { User } from 'src/user/types/user.types';
 import { Comment } from 'src/comment/types/comment.types';
 
-@ObjectType()
+@ObjectType({ description: '게시글을 나타내는 객체' })
 export class Post {
-  @Field(() => ID)
+  @Field(() => ID, { description: '게시글의 고유 식별자' })
   id: number;
 
-  @Field()
+  @Field({ description: '게시글의 제목' })
   title: string;
 
-  @Field()
+  @Field({ description: '게시글의 내용' })
   content: string;
 
-  @Field(() => Int)
+  @Field(() => Int, { description: '게시글 작성자의 ID' })
   authorId: number;
 
-  @Field(() => User)
+  @Field(() => User, { description: '게시글의 작성자 정보' })
   author: User;
 
-  @Field(() => [Comment], { nullable: 'itemsAndList' }) // Lazy loading for relations
+  @Field(() => [Comment], {
+    nullable: 'itemsAndList',
+    description: '게시글에 달린 댓글들',
+  })
   comments: Comment[];
 
-  @Field(() => Int, { nullable: true })
+  @Field(() => Int, {
+    nullable: true,
+    description: '게시글의 카테고리 ID (없을 수 있음)',
+  })
   categoryId?: number;
 
-  @Field(() => Category, { nullable: true })
+  @Field(() => Category, {
+    nullable: true,
+    description: '게시글의 카테고리 정보 (없을 수 있음)',
+  })
   categories?: Category;
 
-  @Field()
+  @Field({ description: '게시글이 생성된 날짜 및 시간' })
   createdAt: Date;
 
-  @Field()
+  @Field({ description: '게시글이 마지막으로 업데이트된 날짜 및 시간' })
   updatedAt: Date;
 }
