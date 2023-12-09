a6d18ff72b4a134c7d4843848440857eab74faab
munawiki <munawiki@gmail.com>
2023-12-09 13:58:41 +0900

    🔧 fix(graphql-error-formatter.ts): refactor EnhancedErrorFormatter class to improve code readability and maintainability 🚀 feat(graphql-error-formatter.ts): add support for mapping Prisma error codes to custom error codes for better error handling and user-friendly messages

--- src/common/graphql-error-formatter.ts
+++ src/common/graphql-error-formatter.ts
@@ -1,28 +1,22 @@
+// path/filename: src/enhanced-error-formatter.ts
 import { GraphQLError } from 'graphql';
 import { LoggerService } from './logger.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 export class EnhancedErrorFormatter {
-  private logger: LoggerService;
+  constructor(private logger: LoggerService) {}
 
-  constructor(loggerService: LoggerService) {
-    this.logger = loggerService;
-  }
-
-  public formatGraphQLError(error: GraphQLError) {
-    const { message, locations, path, extensions } = error;
-
+  public formatGraphQLError(error: GraphQLError): any {
+    const { message, locations, path } = error;
     const customErrorCode = this.determineErrorCode(error);
+    const userFriendlyMessage =
+      this.generateUserFriendlyMessage(customErrorCode);
 
-    this.logger.logError({
-      message,
-      locations,
-      path,
-      errorCode: customErrorCode,
-      originalError: error,
-    });
+    this.logErrorDetails(error, customErrorCode);
 
     return {
-      message,
+      message: this.formatErrorMessage(message, customErrorCode),
+      userFriendlyMessage,
       locations,
       path,
       code: customErrorCode,
@@ -30,13 +24,58 @@ export class EnhancedErrorFormatter {
   }
 
   private determineErrorCode(error: GraphQLError): string {
-    const errorCode = error.extensions?.code;
-    if (typeof errorCode === 'string') {
-      return errorCode === 'INTERNAL_SERVER_ERROR'
-        ? 'BUSINESS_CRITICAL_ERROR'
-        : errorCode;
+    const prismaError = error.originalError as PrismaClientKnownRequestError;
+    const errorCode =
+      typeof error.extensions?.code === 'string'
+        ? error.extensions.code
+        : 'UNHANDLED_ERROR';
+
+    if (prismaError?.code) {
+      return this.mapPrismaErrorCode(prismaError.code);
     }
 
-    return 'UNHANDLED_ERROR';
+    return errorCode === 'INTERNAL_SERVER_ERROR'
+      ? 'BUSINESS_CRITICAL_ERROR'
+      : errorCode;
+  }
+
+  private mapPrismaErrorCode(prismaErrorCode: string): string {
+    const prismaErrorMapping: Record<string, string> = {
+      P2002: 'UNIQUE_CONSTRAINT_FAILED',
+      P2003: 'FOREIGN_KEY_CONSTRAINT_FAILED',
+      // Add more detailed Prisma error mappings here
+    };
+
+    return prismaErrorMapping[prismaErrorCode] || 'DATABASE_ERROR';
+  }
+
+  private formatErrorMessage(message: string, errorCode: string): string {
+    return errorCode === 'BUSINESS_CRITICAL_ERROR'
+      ? `Critical: ${message}`
+      : message;
+  }
+
+  private generateUserFriendlyMessage(errorCode: string): string {
+    switch (errorCode) {
+      case 'UNIQUE_CONSTRAINT_FAILED':
+        return 'A unique constraint was violated. Please verify your input.';
+      case 'FOREIGN_KEY_CONSTRAINT_FAILED':
+        return 'A foreign key constraint was violated. Please check related records.';
+      case 'DATABASE_ERROR':
+        return 'A database error occurred. We are working to resolve this issue.';
+      default:
+        return 'An unexpected error occurred. Our team is looking into it.';
+    }
+  }
+
+  private logErrorDetails(error: GraphQLError, errorCode: string): void {
+    const { message, locations, path } = error;
+    this.logger.logError({
+      message,
+      locations,
+      path,
+      errorCode,
+      originalError: error,
+    });
   }
 }
