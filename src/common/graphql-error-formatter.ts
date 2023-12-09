d9319db66d9bf2f30cb7b585e95d2a1bdfb371f5
munawiki <munawiki@gmail.com>
2023-12-09 19:01:22 +0900

    🐛 fix(graphql-error-formatter.ts): remove unused import and rename path/filename comment to class comment ✨ feat(graphql-error-formatter.ts): add support for mapping Prisma error codes to custom error codes for better error handling and user-friendly messages

--- src/common/graphql-error-formatter.ts
+++ src/common/graphql-error-formatter.ts
@@ -1,13 +1,25 @@
-// path/filename: src/enhanced-error-formatter.ts
 import { GraphQLError } from 'graphql';
 import { LoggerService } from './logger.service';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
+type PrismaErrorCode = 'P2002' | 'P2003'; // Extend as needed
+type ErrorCode =
+  | 'UNIQUE_CONSTRAINT_FAILED'
+  | 'FOREIGN_KEY_CONSTRAINT_FAILED'
+  | 'DATABASE_ERROR'
+  | 'BUSINESS_CRITICAL_ERROR'
+  | 'UNHANDLED_ERROR';
+
 export class EnhancedErrorFormatter {
+  private prismaErrorMapping: Record<PrismaErrorCode, ErrorCode> = {
+    P2002: 'UNIQUE_CONSTRAINT_FAILED',
+    P2003: 'FOREIGN_KEY_CONSTRAINT_FAILED',
+    // Add more detailed Prisma error mappings here
+  };
+
   constructor(private logger: LoggerService) {}
 
   public formatGraphQLError(error: GraphQLError): any {
-    const { message, locations, path } = error;
     const customErrorCode = this.determineErrorCode(error);
     const userFriendlyMessage =
       this.generateUserFriendlyMessage(customErrorCode);
@@ -15,47 +27,45 @@ export class EnhancedErrorFormatter {
     this.logErrorDetails(error, customErrorCode);
 
     return {
-      message: this.formatErrorMessage(message, customErrorCode),
+      ...this.extractErrorDetails(error),
+      message: this.formatErrorMessage(error.message, customErrorCode),
       userFriendlyMessage,
-      locations,
-      path,
       code: customErrorCode,
     };
   }
 
-  private determineErrorCode(error: GraphQLError): string {
-    const prismaError = error.originalError as PrismaClientKnownRequestError;
+  private determineErrorCode(error: GraphQLError): ErrorCode {
     const errorCode =
       typeof error.extensions?.code === 'string'
         ? error.extensions.code
-        : 'UNHANDLED_ERROR';
+        : undefined;
 
-    if (prismaError?.code) {
-      return this.mapPrismaErrorCode(prismaError.code);
+    if (error.originalError instanceof PrismaClientKnownRequestError) {
+      return this.mapPrismaErrorCode(error.originalError.code);
     }
-
-    return errorCode === 'INTERNAL_SERVER_ERROR'
+    return this.isInternalError(errorCode)
       ? 'BUSINESS_CRITICAL_ERROR'
-      : errorCode;
+      : 'UNHANDLED_ERROR';
   }
 
-  private mapPrismaErrorCode(prismaErrorCode: string): string {
-    const prismaErrorMapping: Record<string, string> = {
-      P2002: 'UNIQUE_CONSTRAINT_FAILED',
-      P2003: 'FOREIGN_KEY_CONSTRAINT_FAILED',
-      // Add more detailed Prisma error mappings here
-    };
-
-    return prismaErrorMapping[prismaErrorCode] || 'DATABASE_ERROR';
+  private mapPrismaErrorCode(prismaErrorCode: string): ErrorCode {
+    return (
+      this.prismaErrorMapping[prismaErrorCode as PrismaErrorCode] ||
+      'DATABASE_ERROR'
+    );
   }
 
-  private formatErrorMessage(message: string, errorCode: string): string {
+  private isInternalError(code?: string | undefined): boolean {
+    return code === 'INTERNAL_SERVER_ERROR';
+  }
+
+  private formatErrorMessage(message: string, errorCode: ErrorCode): string {
     return errorCode === 'BUSINESS_CRITICAL_ERROR'
       ? `Critical: ${message}`
       : message;
   }
 
-  private generateUserFriendlyMessage(errorCode: string): string {
+  private generateUserFriendlyMessage(errorCode: ErrorCode): string {
     switch (errorCode) {
       case 'UNIQUE_CONSTRAINT_FAILED':
         return 'A unique constraint was violated. Please verify your input.';
@@ -68,12 +78,14 @@ export class EnhancedErrorFormatter {
     }
   }
 
-  private logErrorDetails(error: GraphQLError, errorCode: string): void {
+  private extractErrorDetails(error: GraphQLError) {
     const { message, locations, path } = error;
+    return { message, locations, path };
+  }
+
+  private logErrorDetails(error: GraphQLError, errorCode: ErrorCode): void {
     this.logger.logError({
-      message,
-      locations,
-      path,
+      ...this.extractErrorDetails(error),
       errorCode,
       originalError: error,
     });
