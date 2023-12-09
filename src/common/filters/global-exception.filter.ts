8608d096e1ab268f9a6da5a12fc62dade12ab7f6
munawiki <munawiki@gmail.com>
2023-12-09 19:28:51 +0900

    🔧 fix(global-exception.filter.ts): enhance HttpExceptionFilter to standardize response format and add error logging
    📝 WHAT:
    - Added a logger to the HttpExceptionFilter class to log caught exceptions.
    - Modified the catch() method to handle both REST and GraphQL requests.
    - Standardized the response format for both REST and GraphQL requests.
    - Added additional error logging for caught exceptions.
    - For GraphQL requests, throw a formatted error.
    - For REST requests, send a formatted response.
    
    🚀 WHY:
    - The logger was added to provide better visibility into caught exceptions and aid in debugging.
    - The response format was standardized to provide a consistent structure for error responses across REST and GraphQL requests.
    - The additional error logging was added to capture more information about the caught exceptions.
    - Throwing a formatted error for GraphQL requests ensures that the error is properly handled by the GraphQL layer.
    - Sending a formatted response for REST requests ensures that the error is properly communicated to the client.

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -1,17 +1,53 @@
+// /path/to/http-exception.filter.ts
 import {
   ExceptionFilter,
   Catch,
   ArgumentsHost,
   HttpException,
+  Logger,
 } from '@nestjs/common';
 import { GqlArgumentsHost, GqlExceptionFilter } from '@nestjs/graphql';
+import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter
   implements ExceptionFilter, GqlExceptionFilter
 {
+  private readonly logger = new Logger(HttpExceptionFilter.name);
+
   catch(exception: HttpException, host: ArgumentsHost) {
     const gqlHost = GqlArgumentsHost.create(host);
-    return exception;
+    const context = gqlHost.getContext();
+    const response: Response = context.res;
+    const request: Request = context.req;
+
+    // Standardizing response format for both REST and GraphQL
+    const { status, message } = exception.getResponse() as {
+      status: number;
+      message: string[] | string;
+    };
+
+    // Enhanced error response format
+    const errorResponse = {
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request?.url, // Extracting URL from REST or GraphQL request
+      message: Array.isArray(message) ? message : [message],
+    };
+
+    // Additional error logging
+    this.logger.error({
+      message: 'Exception caught',
+      exception: errorResponse,
+      context: request ? 'REST' : 'GraphQL',
+    });
+
+    // For GraphQL requests, throw a formatted error
+    if (!response) {
+      throw new HttpException(errorResponse, status);
+    }
+
+    // For REST requests, send a formatted response
+    response.status(status).json(errorResponse);
   }
 }
